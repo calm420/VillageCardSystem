819e87ebf28d1b06ed52281c5dfb41f056728585
john_changliang <john_changliang@163.com>
2018-10-25 10:43:56 +0800

    班牌项目初始化

---
+++ static/header/asset/js/header.js
@@ -0,0 +1,66 @@
+$(function () {
+
+    var article = {};
+    article.attacheMents = [];
+    InitializePage();
+
+    $('#changeImage').click(function () {
+        $("#upload").change(function () {
+            if (this.files[0]) {
+                var formData = new FormData();
+                formData.append("file" + 0, this.files[0]);
+                formData.append("name" + 0, this.files[0].name);
+                $.ajax({
+                    type: "POST",
+                    url: "https://jiaoxue.maaee.com:8890/Excoord_Upload_Server/file/upload",
+                    enctype: 'multipart/form-data',
+                    data: formData,
+                    // 告诉jQuery不要去处理发送的数据
+                    processData: false,
+                    // 告诉jQuery不要去设置Content-Type请求头
+                    contentType: false,
+                    success: function (res) {
+                    }
+                });
+            }
+        })
+    })
+
+    /**
+     * 时间戳转年月日
+     * @param nS
+     * @returns {string}
+     */
+    formatYMD = function (nS) {
+        var da = new Date(parseInt(nS));
+        var year = da.getFullYear();
+        var month = da.getMonth() + 1;
+        var date = da.getDate();
+        var ymdStr = [year, month, date].join('-');
+        return ymdStr;
+    };
+
+    function sendMessageTo(data) {
+        window.parent.postMessage(JSON.stringify(data), '*');
+    }
+
+    //监听接受消息
+    window.addEventListener('message', (e) => {
+        alert(e);
+        var res = JSON.parse(e.data);
+        if (res.method == 'test') {
+            console.log(res, '测试的postMessage');
+        } else if (res.method == 'clearRichTestSign') {
+            //清空编辑器内容
+            window.location.reload();
+        } else if (res.method == 'closeMask') {
+
+        }
+    })
+
+    //初始化页面元素
+    function InitializePage() {
+
+    }
+
+})
