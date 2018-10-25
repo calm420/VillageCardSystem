29980314906e644bea7f72d759ade262ad65ecfa
noom <noom0130@foxmail.com>
2018-10-25 22:53:43 +0800

    .

--- static/tableItemDetil/asset/js/tableItemDetil.js
+++ static/tableItemDetil/asset/js/tableItemDetil.js
@@ -1,6 +1,43 @@
 $(function () {
     var roomId = getQueryString("roomId");
-    console.log(roomId);
+    viewCourseTable(roomId)
+
+
+    function viewCourseTable(roomId) {
+        var param = {
+            "method": 'viewRoomCourseTable',
+            "rid": roomId,
+        };
+        WebServiceUtil.requestLittleAntApi(true, JSON.stringify(param), {
+            onResponse: function (result) {
+                if (result.msg == '调用成功' || result.success == true) {
+                    buileTable(result.response)
+                }
+            },
+            onError: function (error) {
+                // message.error(error);
+            }
+        });
+    }
+
+    function buileTable(data) {
+        var tbody = ''
+        var td;
+        for (var k in data) {
+            var tr = '';
+            data[k].forEach((e) => {
+                if (!!e.courseName) {
+                    td = '<td><span>' + e.courseName + '</span><span>' + e.classRoom.name + '</span> <span>(' + e.openTime + '-' + e.closeTime + ')</span> </td>'
+                } else {
+                    td = '<td></td>'
+                }
+                tr += td;
+            })
+            var trs = '<tr><td>' + k + '</td>' + tr + '</tr>'
+            tbody += trs
+        }
+        document.querySelector('tbody').innerHTML = tbody
+    }
 
     /**
      * 获取地址栏参数
