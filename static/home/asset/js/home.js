a282d278d28e39bca2ef11705f48f0768cf256bb
noom <noom0130@foxmail.com>
2018-10-26 23:04:58 +0800

    .

--- static/home/asset/js/home.js
+++ static/home/asset/js/home.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
     simpleMs.connect();
 
     var isDebug = false;
-    var webserviceUrl = isDebug ? "http://192.168.50.29:7091/" : "https://jiaxue.maaee.com:8092/";
+    var webserviceUrl = isDebug ? "http://192.168.50.29:7091/" : "https://jiaoxue.maaee.com:9092/";
 
     InitializePage();
 
