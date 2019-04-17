06a4907794919569840cee0727a710b4a0183c31
noom <noom0130@foxmail.com>
2019-04-17 09:20:40 +0800

    .

--- static/js/webServiceUtil.js
+++ static/js/webServiceUtil.js
@@ -1,15 +1,15 @@
 //本地调试时,需要设置为true,保证ifream的跳转路径为本地地址,发版时一定要调整为false
 WebServiceUtil.isDebug_ifream = true;
 //请求api的debug设置,做本地调试,调用本地接口时,需要设置为true,否则为false,发版时一定要调整为false
-WebServiceUtil.isDebug = true;
+WebServiceUtil.isDebug = false;
 //simple消息服务的debug设置,需要调用本地simple消息服务时,需要设置为true,否则为false,发版时一定要调整为false
 WebServiceUtil.isDebug_simpleScoket = false;
 //message消息服务的debug设置,需要调用本地message消息服务时,需要设置为true,否则为false,发版时一定要调整为false
 WebServiceUtil.isDebug_messageScoket = false;
 
-WebServiceUtil.localDomain = "192.168.50.139";   //请求地址接口
+WebServiceUtil.localDomain = "192.168.50.15";   //请求地址接口
 
-WebServiceUtil.localDebugUrl = "192.168.50.72";   //本地调试的地址,嵌套ifream页面使用的地址
+WebServiceUtil.localDebugUrl = "192.168.50.30";   //本地调试的地址,嵌套ifream页面使用的地址
 
 //小蚂蚁webService地址
 var apiWebServiceURLOfLocals = "http://" + WebServiceUtil.localDomain + ":9006/Excoord_ApiServer/webservice";
