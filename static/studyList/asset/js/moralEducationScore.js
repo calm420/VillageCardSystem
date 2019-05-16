f12c32fa387ce30288a879cc30baba25e5755e0f
saikie <saikie.gmail.com>
2019-05-16 21:43:05 +0800

    --

--- static/studyList/asset/js/moralEducationScore.js
+++ static/studyList/asset/js/moralEducationScore.js
@@ -32,8 +32,8 @@ $(function () {
             onResponse: function (result) {
                 console.log(result, "result");
                 if (result.msg == '调用成功' || result.success == true) {
-                    if (result.response == null) {
-                        $(".mEScoreInfo").replaceWith('<div class="mEScoreInfo home_cardCont"><div class="empty_center"><div class="empty_icon empty_moralEducationScore"></div><div class="empty_text">暂无评分</div></div></div>');
+                    if (result.response.length === 0) {
+                        // $(".mEScoreInfo").replaceWith('<div class="mEScoreInfo home_cardCont"><div class="empty_center"><div class="empty_icon empty_moralEducationScore"></div><div class="empty_text">暂无评分</div></div></div>');
                     } else {
                         let str = '';
                         for (let i = 0; i < result.response.length; i++) {
